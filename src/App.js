f9fa747c28d120111c41eace284b0e4bbe313324
reets <117296082+reets93@users.noreply.github.com>
2023-03-16 21:06:15 +0000

    added activity page folder, created map of activity component. added card detail json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Bored from "./components/Bored";
 import './App.css';
-import ActivityCard from "./components/ActivityCard/ActivityCard";
+// import ActivityCard from "./components/ActivityCard/ActivityCard";
+import ActivityPage from "./components/ActivityPage/ActivityPage";
 
 function App() {
   return (
     <div>
       <Bored />
-      <ActivityCard />
+      <ActivityPage />
     </div>
   )
 }
