0b0fde4a1204c3ea79e2a45a4da07fa7884dde37
reets <117296082+reets93@users.noreply.github.com>
2023-03-16 18:40:16 +0000

    created activity card component, css and json files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Bored from "./components/Bored";
 import './App.css';
+import ActivityCard from "./components/ActivityCard/ActivityCard";
 
 function App() {
-  return(
-  <Bored/>
+  return (
+    <div>
+      <Bored />
+      <ActivityCard />
+    </div>
   )
 }
- 
+
 export default App;
 
 
