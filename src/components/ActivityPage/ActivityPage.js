03699c018fb60bfea02190cecadaf308e323d4e0
reets <117296082+reets93@users.noreply.github.com>
2023-03-17 21:00:16 +0000

    added icon props for the react icon ready to be added to json file

--- src/components/ActivityPage/ActivityPage.js
+++ src/components/ActivityPage/ActivityPage.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import ActivityCard from "../ActivityCard/ActivityCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import cardDetails from '../ActivityCard/CardImage.json'
-import Bored from "../Bored";
+// import Bored from "../Bored";
+// import react icons here?
 
 function ActivityPage(props) { //pulling in bored data as a prop from app which will be useable here. 
     const boredData = props.boredData
@@ -10,6 +11,8 @@ function ActivityPage(props) { //pulling in bored data as a prop from app which 
 
 for (let i=0; i < boredData.length; i++) {
    boredData[i].image = cardDetails[i].image 
+   boredData[i].type = cardDetails[i].type
+   boredData[i].icon = cardDetails[i].icon
 }
 
     return (
@@ -18,6 +21,7 @@ for (let i=0; i < boredData.length; i++) {
                 return (
                     <div className="activities">
                         <ActivityCard
+                            icon={d.icon}
                             type={d.type}
                             image={d.image}
                             activity={d.activity}
