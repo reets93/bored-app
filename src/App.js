798f55aa418f4ef46bc300481827d61c23797ece
reets <117296082+reets93@users.noreply.github.com>
2023-03-17 16:01:04 +0000

    updated hierarchy of bored api. moved to app.js so that it can be accessed in activity page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { useState } from "react";
-import Bored from "./components/Bored";
+// import Bored from "./components/Bored";
 import Youtube from "./components/Youtube";
 import './App.css';
 // import ActivityCard from "./components/ActivityCard/ActivityCard";
 import ActivityPage from "./components/ActivityPage/ActivityPage";
+import Axios from "axios";
 
 function App() {
+  const [boredData, setBoredData] = useState({}); // state that handles the bored api data
+  const baseUrl = "http://www.boredapi.com/api/activity?type=";
+
+  const queryArray = ["social", "recreational", "education", "diy", "charity", "cooking", "relaxation", "music", "busywork"]; // listing out the category names so that later we can call the api in a for loop with the different type as the query
+
+  const getCurrentActivites = () => { //will be an onclick function --> two diff components (try this button; and try more)
+    const activityArray = [];// will push api data objects to this array for each query search result
+
+    queryArray.map(q => {
+      // console.log("i'm looping")
+      // console.log(typeof q)
+      // console.log(q)
+      Axios.get(baseUrl + q)
+        .then((response) => {
+          // console.log(response);
+          activityArray.push(response.data) // pushing result to activityArray
+          console.log(activityArray)
+          // activityArray.length === 9 && setBoredData(activityArray)
+          
+          return activityArray
+        });
+    })
+    console.log(typeof activityArray)
+    // setBoredData(activityArray);  // failing here - doesn't want to work -- once return an object, want to set our state to that , semantics or use state issue?
+    // console.log(boredData);
+  }
+
+
   return (
     <div>
-      <Bored />
-      <ActivityPage />
+      {/* <Bored /> */}
+      <button onClick={getCurrentActivites}>Try This - Testing</button>
+      {/* {boredData.length > 0 && boredData[0].activity}
+      {boredData.length > 0 && boredData[8].activity} */}
+      <ActivityPage boredData={boredData} fetchNewData={getCurrentActivites} /> {/* passes down through to other page so i can access and use the data there as a prop */}
     </div>
   )
 }
 
 export default App;
-
-
-
--- src/components/ActivityPage/ActivityPage.js
+++ src/components/ActivityPage/ActivityPage.js
@@ -1,26 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ActivityCard from "../ActivityCard/ActivityCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import cardDetails from  '../ActivityCard/CardImage.json'
+import cardDetails from '../ActivityCard/CardImage.json'
 import Bored from "../Bored";
 
-function ActivityPage() {
-const [details, setDetails] = useState(cardDetails)
-const [result, setResult] = useState(Bored) // not sure if this is needed/ does anything?
+function ActivityPage(boredData) { //pulling in bored data as a prop from app which will be useable here. 
+    const [details, setDetails] = useState(cardDetails)
 
-    return(
-<div>
-{details.map(d => {
-    return(
-    <div className="activities"> 
-    <ActivityCard
-    type={d.type}
-    image={d.image}
-    activity={d.activity}/> 
-    </div>)//where does the activity come through from? link it to the API output
-  })}
-
-</div>
+    return (
+        <div>
+            {details.map(d => { //linking to cardDetails ; need to figure out how to then link the borred API data 
+                return (
+                    <div className="activities">
+                        <ActivityCard
+                            type={d.type}
+                            image={d.image}
+                        // activity= {boredData.activity.map(enter logic here)} // find activity name based on card detail index
+                        /* should work once the setData works*/
+                        />
+                    </div>
+                )//where does the activity come through from? link it to the API output
+            })}
+        </div>
     )
 }
 
