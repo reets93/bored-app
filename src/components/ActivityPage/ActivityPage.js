0b95a2ac55adec7f72e70a64475bfdcca29e9aa7
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-19 16:24:51 +0000

    cards in a 3 x 3 grid

--- src/components/ActivityPage/ActivityPage.js
+++ src/components/ActivityPage/ActivityPage.js
@@ -2,6 +2,8 @@ import React from "react";
 import ActivityCard from "../ActivityCard/ActivityCard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import cardDetails from '../ActivityCard/CardImage.json'
+import Row from "react-bootstrap/Row"
+
 
 
 function ActivityPage(props) { //pulling in boredData as a prop from app which will be useable here. 
@@ -14,7 +16,9 @@ for (let i=0; i < boredData.length; i++) {
 }
 
     return (
-        <div>
+        //Added lines 20 and 21 to get cards into a grid of 9 - but now activity text is missing from last 6 cards and I don't know why?
+        <div className="cards">
+      <Row xs={4} md={3} className="g-3">
             {boredData.map(d => { 
                 return (
                     <div className="activities">
@@ -27,6 +31,7 @@ for (let i=0; i < boredData.length; i++) {
                     </div>
                 )
             })}
+            </Row>
         </div>
     )
 }
