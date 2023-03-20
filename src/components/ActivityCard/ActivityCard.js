6155f901aef94b3887c69581575a9fb862cd3021
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-20 18:29:02 +0000

    hover effect added to cards; page styling

--- src/components/ActivityCard/ActivityCard.js
+++ src/components/ActivityCard/ActivityCard.js
@@ -5,20 +5,20 @@ import { FaBinoculars } from "react-icons/fa";
 
 function ActivityCard(props) {
     return (
-        <div id='activity-card'className="activity-card">
-            <div className="card" >
-                <img src={props.image} alt=""
-                className="opacity-50" width = "340px" height = "240px"></img>
-                <div className="card-img-overlay">
-                    <h4 className="title">{props.type}</h4>
-                    <p className="card-text">{props.activity}</p>
-                    <div className="d-flex justify-content-center">
-                    <button className='btn btn-primary btn-default'> Explore <FaBinoculars /></button>
-                   </div>
-                </div>
-                </div>
-            </div>
+        <div className="activity-card">
+       <div className="cardActivity">
+        <img src={props.image} class="card-img" alt="" width="340px" height="240px"></img>
+        <div className="card-main">
+        <h4 className="card-title">{props.type}</h4>
+        <p className="card-info">{props.activity}</p>
+        <div className="d-flex justify-content-center">
+        <button className='cardBtn'> Explore <FaBinoculars /></button>
+        </div>
+        </div>
+        </div>
+       </div>
     )
 }
 
 export default ActivityCard
+
