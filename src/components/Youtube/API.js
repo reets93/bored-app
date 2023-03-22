598d78864f25394047a7ff8e153013fa6f122cf0
kamel beloula <kamel.solo@live.com>
2023-03-22 01:17:27 +0000

    added a youtube Api key specially for the presentation

--- src/components/Youtube/API.js
+++ src/components/Youtube/API.js
@@ -5,9 +5,9 @@ const KEY =
             "AIzaSyDNe-EwKY81Imo8_AyAvMBnKbvaBYFqbNs";
             // "AIzaSyAdG72xSeFTf3zesij_Rztmomuamtbg9hQ";
             // "AIzaSyDGCWZu84Wo1O7MmVU6sHP67CzIOQyaz_E";
-            //******************************************* */
+            //
             //"AIzaSyCeXpc-R0ZBzZttfZsRtMFysAmK9xPhheQ"; "DO NOT USE" Keep this one for the presentation
-            //****************************************** */
+            
          
 // axios api requesst with the parameters            
 export default axios.create({
