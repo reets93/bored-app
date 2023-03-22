3d224e9c43c78033af58c480ff74f12bedc43eb0
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-22 22:04:13 +0000

    Search Bar working

--- src/components/Youtube/SearchBar.js
+++ src/components/Youtube/SearchBar.js
@@ -1,35 +1,34 @@
-import React, { useState } from "react";
-
-//searchbar function using the ontermsubmit juction passed from the youtube.js
-function SearchBar(props) {
-
-  // define the state term and initialse it as an empty string
-  const [term, setTerm] = useState("");
-
-  // form components returned
-  return (
-    <div className="container-fluid">
-      <form className="form-inline" onSubmit={props.onTermSubmit}>
-        <div className="form-group mb-2">
-          <label for="searchBar" className="sr-only">
-            Youtube search
-          </label>
-          <input
-            className="form-control"
-            placeholder='Enter your keyword and then press "Enter"'
-            type="text"
-            value={term}
-            // the setTerm updates the term state in the ontermsubmit in the api request in the youtube.js
-            onChange={(e) => setTerm({ term: e.target.value })}
-          />
-          <small className="form-text text-muted">
-            Find a video about your chosen activity
-          </small>
-        </div>
-      </form>
-    </div>
-  );
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+class SearchBar extends React.Component {
+  state = { term: "" };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onTermSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div className="container-fluid">
+        <form className="form-inline" onSubmit={this.onFormSubmit}>
+          <div className="form-group mb-2">
+            <label for="searchBar" className="sr-only">
+              Youtube search
+            </label>
+            <input
+              className="form-control"
+              placeholder='Enter your keyword and then press "Enter"'
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+            <small className="form-text text-muted">
+              Find a video about your chosen activity
+            </small>
+            {/* <button className='btn-light 'onClick={this.onFormSubmit}> Search<FaSearch/></button> */}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
-
-
 export default SearchBar;
