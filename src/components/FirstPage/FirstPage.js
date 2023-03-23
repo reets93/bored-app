0d4890183e4588cfd3389b43b029a72ce0812348
lissasimp <116848303+lissasimp@users.noreply.github.com>
2023-03-23 10:19:28 +0000

    click here button responsive

--- src/components/FirstPage/FirstPage.js
+++ src/components/FirstPage/FirstPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 import Animation from '../Animation/Animation';
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
@@ -14,11 +17,17 @@ function FirstPage(props) {
     <div >
       <div id="frame">
         <Animation></Animation>
+        <Container className="btnContainer">
+          <Row>
+            <Col className="text-center" style ={{ marginTop: "30em"}}>
         <Button id='button' onClick={props.getCurrentActivites}
         >
           <span><FaArrowRight />  Click Here  <FaArrowLeft />
           </span>
         </Button>
+        </Col>
+          </Row>
+      </Container>
         <p id='quote'>"There are no uninteresting things, only uninterested people."
           G.K. Chesterton</p>
       </div>
